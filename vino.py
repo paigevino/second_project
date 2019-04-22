216a7430ba75c04fc0d34d0c97d949e705d5d0c3
paigevino <vinodhinip2909@gmail.com>
2019-04-22 11:03:37 +0530

    commit by vino

--- vino.py
+++ vino.py
@@ -1,8 +1,8 @@
 from flask import Flask ,render_template,request
 #import mysql.connector as db
 app = Flask(__name__)
-#@app.route('/')
-#def student():
- #   return render_template('student.html')
+@app.route('/')
+def student():
+   return render_template('student.html')
 if __name__=="__main__":
    app.run()
